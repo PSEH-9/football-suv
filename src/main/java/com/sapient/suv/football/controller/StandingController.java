d764bc18cc39af693cf39563423b329920b0f5f2
Hiring <Hiring@WKWIN4317494.global.publicisgroupe.net>
2019-07-13 14:42:26 +0530

    input validation

--- src/main/java/com/sapient/suv/football/controller/StandingController.java
+++ src/main/java/com/sapient/suv/football/controller/StandingController.java
@@ -3,10 +3,12 @@ package com.sapient.suv.football.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.sapient.suv.football.domain.Standing;
 import com.sapient.suv.football.service.StandingService;
@@ -21,7 +23,22 @@ public class StandingController {
 	@GetMapping
 	public ResponseEntity<Standing> getStanding(@RequestParam("countryName") String countryName,
 			@RequestParam("leagueName") String leagueName, @RequestParam("teamName") String teamName) {
+		validate(countryName, leagueName, teamName);
 		return new ResponseEntity<>(standingService.getStandings(countryName, leagueName, teamName), HttpStatus.OK);
+	}
+
+	private void validate(String countryName, String leagueName, String teamName) {
+		if (StringUtils.isEmpty(countryName)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Country Name required .");
+		}
+
+		if (StringUtils.isEmpty(leagueName)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "League Name required .");
+		}
+
+		if (StringUtils.isEmpty(teamName)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Team Name required");
+		}
 	}
 
 }
